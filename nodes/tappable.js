9e41e26ae326ef5013b7c42c1bf0b6aee8c46248
Marcus Westin <marcus.westin@gmail.com>
2012-02-07 17:45:38 -0800

    Don't mark the scrollable tappable items as active right away - wait a short moment

--- nodes/tappable.js
+++ nodes/tappable.js
@@ -1,6 +1,7 @@
 var Rect = require('std/math/Rect'),
 	curry = require('std/curry'),
-	client = require('std/client')
+	client = require('std/client'),
+	bind = require('std/bind')
 
 module.exports = {
 	withScroll: withScroll,
@@ -8,18 +9,18 @@ module.exports = {
 }
 
 function withScroll(tapHandler) {
-	return tappable(allowScrollHandler, tapHandler)
+	return tappable(allowScroll, tapHandler)
 }
 
 function withoutScroll(tapHandler) {
-	return tappable(preventScrollHandler, tapHandler)
+	return tappable(preventScroll, tapHandler)
 }
 
-function tappable(touchMoveHandler, tapHandler) {
+function tappable(handlers, tapHandler) {
 	return (client.isMobile
 		? {
-			touchstart: onTouchStart,
-			touchmove: touchMoveHandler,
+			touchstart: handlers.onTouchStart,
+			touchmove: handlers.onTouchMove,
 			touchend: curry(endTouch, tapHandler),
 			touchcancel: curry(endTouch, tapHandler)
 		} : {
@@ -30,43 +31,59 @@ function tappable(touchMoveHandler, tapHandler) {
 		})
 }
 
-function onTouchStart(e) {
-	// TODO Don't listen to move, end or cancel until touch start
-	if (e.touches.length > 1) { return }
-	// if (this.hasClass('disabled')) { return }
-	var offset = this.getOffset()
-	this.__touchRect = new Rect(offset.left, offset.top, offset.width, offset.height).pad(3)
-	this.addClass('active')
-}
-
-function allowScrollHandler(e) {
-	if (!this.__touchRect) { return }
-	var touch = e.touches[0]
-	if (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) { return }
-	document.getElementsByClassName('logout')[0].innerHTML = "ASDASD"
-	this.removeClass('active')
-	delete this.__touchRect
-}
-
-function preventScrollHandler(e) {
-	if (!this.__touchRect) { return }
-	e.cancel()
-	var touch = e.touches[0]
-	if (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) {
+var preventScroll = {
+	onTouchStart: function(e) {
+		// TODO Don't listen to move, end or cancel until touch start
+		if (e.touches.length > 1) { return }
+		// if (this.hasClass('disabled')) { return }
+		var offset = this.getOffset()
+		this.__touchRect = new Rect(offset.left, offset.top, offset.width, offset.height).pad(10)
 		this.addClass('active')
-	} else {
-		this.removeClass('active')
+	},
+	onTouchMove: function(e) {
+		if (!this.__touchRect) { return }
+		e.cancel()
+		var touch = e.touches[0]
+		if (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) {
+			this.addClass('active')
+		} else {
+			this.removeClass('active')
+		}
+	}
+}
+
+var allowScroll = {
+	onTouchStart: function(e) {
+		// TODO Don't listen to move, end or cancel until touch start
+		if (e.touches.length > 1) { return }
+		// if (this.hasClass('disabled')) { return }
+		var touch = e.touches[0]
+		this.__touchRect = new Rect(touch.pageX, touch.pageY, 0, 0).pad(4)
+		this.__activeDelayTimeout = setTimeout(bind(this, function() {
+			this.addClass('active')
+		}), 135)
+	},
+	onTouchMove: function(e) {
+		if (!this.__touchRect) { return }
+		var touch = e.touches[0]
+		if (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) { return }
+		clearState.apply(this)
 	}
 }
 
 function endTouch(tapHandler, e) {
 	if (!this.__touchRect) { return }
-	delete this.__touchRect
 	e.cancel()
 	var shouldTap = this.hasClass('active')
-	this.removeClass('active')
+	clearState.apply(this)
 	if (shouldTap) { tapHandler() }
-	// TODO deregister touch events
+}
+
+function clearState() {
+	delete this.__touchRect
+	clearTimeout(this.__activeDelayTimeout)
+	delete this.__activeDelayTimeout
+	this.removeClass('active')
 }
 
 function setActive() {
