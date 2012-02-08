4da23f8b2ec91e62e02b12aadcc28979c7d54f3b
Marcus Westin <marcus.westin@gmail.com>
2012-02-08 14:24:05 -0800

    Fix up the endTouch handling with a bunch of clover-specific stuff. This should go in a list module

--- nodes/tappable.js
+++ nodes/tappable.js
@@ -56,34 +56,54 @@ var allowScroll = {
 	onTouchStart: function(e) {
 		// TODO Don't listen to move, end or cancel until touch start
 		if (e.touches.length > 1) { return }
+		
 		// if (this.hasClass('disabled')) { return }
 		var touch = e.touches[0]
 		this.__touchRect = new Rect(touch.pageX, touch.pageY, 0, 0).pad(4)
 		this.__activeDelayTimeout = setTimeout(bind(this, function() {
 			this.addClass('active')
-		}), 135)
+		}), 150)
 	},
 	onTouchMove: function(e) {
 		if (!this.__touchRect) { return }
 		var touch = e.touches[0]
 		if (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) { return }
 		clearState.apply(this)
+		this.removeClass('active')
 	}
 }
 
+// HACK this function has a bunch of clover list specific details in it. Abstract it up to a list level
 function endTouch(tapHandler, e) {
 	if (!this.__touchRect) { return }
+	if (e.changedTouches.length > 1) { return }
 	e.cancel()
-	var shouldTap = this.hasClass('active')
-	clearState.apply(this)
-	if (shouldTap) { tapHandler() }
+	setTimeout(bind(this, function() { // Give the scroll event a chance to happen
+		if (gLastScroll && (new Date().getTime() - gLastScroll < 100)) {
+			gLastScroll = null
+			clearState.call(this)
+			this.removeClass('active')
+			return;
+		}
+		var touch = e.changedTouches[0],
+			shouldTap = (this.__touchRect.containsPoint({ x:touch.pageX, y:touch.pageY }))
+		setTimeout(bind(this, this.removeClass, 'active'), 200)
+		if (shouldTap && !this.hasClass('active')) {
+			this.addClass('active')
+			clearState.call(this)
+			if (shouldTap) { setTimeout(tapHandler, 0) }
+		} else {
+			clearState.apply(this)
+			if (shouldTap) { tapHandler() }
+		}
+		
+	}), 50)
 }
 
 function clearState() {
 	delete this.__touchRect
 	clearTimeout(this.__activeDelayTimeout)
 	delete this.__activeDelayTimeout
-	this.removeClass('active')
 }
 
 function setActive() {
