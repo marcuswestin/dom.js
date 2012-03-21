7482d3ccb9b16ee4ae4c14cac613c26a68fa5634
Marcus Westin <marcus.westin@gmail.com>
2012-03-20 17:27:58 -0700

    Use classList if it exists, and implement toggleClass

--- addClass.js
+++ addClass.js
@@ -1,6 +1,10 @@
 var hasClass = require('./hasClass')
 
 module.exports = function(el, className) {
-	if (hasClass(el, className)) { return }
-	el.className += ' ' + className
+	if (el.classList) {
+		el.classList.add(className)
+	} else {
+		if (hasClass(el, className)) { return }
+		el.className += ' ' + className
+	}
 }
