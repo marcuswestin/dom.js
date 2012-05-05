ea703c7989d7ae1d4c551442572047d146b0f0ea
Marcus Westin <marcus.westin@gmail.com>
2012-05-05 13:39:41 -0700

    make the viewcontent always overflow the fixed size native scroll wrapper so that the native scroll is always active

--- scroller.js
+++ scroller.js
@@ -1,7 +1,7 @@
 var viewport = require('dom/viewport')
 
 module.exports = function() {
-	return create(scroller, { stack:[] })
+	return create(scroller, { stack:[], viewContents:[] })
 }
 
 var scroller = {
@@ -12,12 +12,14 @@ var scroller = {
 	},
 	renderBody:function(numViews, renderBodyContent) {
 		this.renderBodyContent = renderBodyContent
-		var viewportSize = viewport.getSize()
-		var contentSize = style({ height:viewport.height()-this.headHeight, width:viewport.width() })
-		var crop = style({ overflowX:'hidden' })
+		var viewSize = { height:viewport.height()-this.headHeight, width:viewport.width() }
+		var viewSized = style(viewSize)
+		// view content size is 1px extra in height to ensure that overflow scrolling is always on
+		// var contentSized = style({ width:viewSize.width, 'min-height':viewSize.height+1 })
+		var cropped = style({ overflowX:'hidden' })
 		var scrollable = style({ 'overflow-y':'scroll', '-webkit-overflow-scrolling':'touch' })
 		var floating = style({ 'float':'left' })
-		var slider = style({
+		var sliderStyle = style({
 			height:viewport.height() - this.headHeight,
 			width:viewport.width() * numViews,
 			'-webkit-transition':'-webkit-transform 0.70s',
@@ -25,10 +27,12 @@ var scroller = {
 		})
 		
 		this.body=div('scroller-body', style({ position:'absolute', top:this.headHeight, overflowX:'hidden' }),
-			div('scroller-overflow', contentSize, crop,
-				this._slider=div('scroller-slider', slider,
-					this.views=map(new Array(numViews), function() {
-						return div('scroller-view', contentSize, crop, floating, scrollable)
+			div('scroller-overflow', viewSized, cropped,
+				this._slider=div('scroller-slider', sliderStyle,
+					map(new Array(numViews), this, function(_, i) {
+						return div('scroller-view', viewSized, cropped, floating, scrollable,
+							this.viewContents[i] = div('scroller-content')
+						)
 					})
 				)
 			)
@@ -37,12 +41,12 @@ var scroller = {
 		return this.body
 	},
 	push:function(newView) {
-		var views = this.views
+		var viewContents = this.viewContents
 		var stack = this.stack
-		var viewBelow = views[stack.length - 1]
+		var viewBelow = viewContents[stack.length - 1]
 		this.stack.push(newView)
 		this.head.empty().append(this.renderHeadContent(newView, viewBelow))
-		views[this.stack.length - 1].empty().append(this.renderBodyContent(newView, viewBelow))
+		viewContents[this.stack.length - 1].empty().append(this.renderBodyContent(newView, viewBelow))
 		this._scroll()
 	},
 	pop:function() {
